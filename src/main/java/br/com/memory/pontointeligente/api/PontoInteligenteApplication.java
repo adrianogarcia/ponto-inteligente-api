5f36c5d95646ad4bf0e8be51874bd92aa78d9f0c
Adriano Garcia <adriano@MEMORY.LOCAL>
2018-08-10 14:06:27 -0300

    Alteração para testes de commit do projeto no git

--- src/main/java/br/com/memory/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/br/com/memory/pontointeligente/api/PontoInteligenteApplication.java
@@ -10,5 +10,9 @@ public class PontoInteligenteApplication
 	public static void main(String[] args)
 	{
 		SpringApplication.run(PontoInteligenteApplication.class, args);
+		
+		System.out.println("Olá meu caro...");
+		
+		
 	}
 }
